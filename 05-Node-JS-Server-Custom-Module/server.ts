811fa82c4eb9d231276dfe9f56101cd4b00cf064
asyrafahmad <are.syraf9764@gmail.com>
2022-12-12 22:55:16 +0800

    add Math Util

--- 05-Node-JS-Server-Custom-Module/server.ts
+++ 05-Node-JS-Server-Custom-Module/server.ts
@@ -4,6 +4,7 @@ import fs from 'fs';        // import File System library
 import path  from 'path';   // import path library
 
 import {StringUtil} from "./util/StringUtil"
+import {MathUtil} from "./util/MathUtil"
 
 const hostname:string = '127.0.0.1'
 const port:number = 5000;
@@ -41,16 +42,21 @@ const server:Server = http.createServer( (request: IncomingMessage, response : S
     // })
 
     /* String Util module */
-    let customerName:string = 'Asyraf Brains';
-    let length:number = StringUtil.printLength(customerName);
-    let channelName:string = 'Asyraf Technologies'
-    let result:string = StringUtil.printTriangle(channelName)
+    // let customerName:string = 'Asyraf Brains';
+    // let length:number = StringUtil.printLength(customerName);
+    // let channelName:string = 'Asyraf Technologies'
+    // let result:string = StringUtil.printTriangle(channelName)
+
+    /* Math Util module */
+    let theNumber:number = 5;
+    let resultNumber:string = MathUtil.printMathTable(theNumber);
 
 
     /* Output */
     // response.end(`<h3 style="font-family: Lato, sans-serif; color:green" > Welcome to the NOde JS server`);
     // response.end(`<pre> ${JSON.stringify(osData)} </pre>`)
-    response.end(`<pre style="font-family: Lato, sans-serif; color:green">Length: ${length} \n\n${result} </pre>`);
+    // response.end(`<pre style="font-family: Lato, sans-serif; color:green">Length: ${length} \n\n${result} </pre>`);
+    response.end(`<pre style="font-family: Lato, sans-serif; color:green">\n\nMath: \n${resultNumber} </pre>`);
 });
 
 server.listen(port, hostname, () => {
